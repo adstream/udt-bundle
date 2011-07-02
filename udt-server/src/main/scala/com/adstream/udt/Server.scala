97ee03aad0fa98d19c7101b590edd126b02717b7
Yaroslav Klymko <t3hnar@gmail.com>
2011-07-02 21:53:11 +0300

    using udt java wrapper

--- udt-server/src/main/scala/com/adstream/udt/Server.scala
+++ udt-server/src/main/scala/com/adstream/udt/Server.scala
@@ -5,17 +5,20 @@ import akka.event.EventHandler
 import akka.actor.Actor._
 import akka.actor.Actor
 import akka.routing._
-import udt.{UDTOutputStream, UDTInputStream, UDTSocket, UDTServerSocket}
 import io.Source
 import java.io._
 import scala.tools.nsc.io.Streamable
 import sun.misc.Resource
 import collection.mutable.ListBuffer
+import com.barchart.udt.{TypeUDT, SocketUDT}
 
 /**
  * @author Yaroslav Klymko
  */
 object Server extends App {
+
+
+
   //  val server = new UDTServerSocket(InetAddress.getByName("localhost"), 65321)
   //  val socket = server.accept()
   //  System.out.println("Processing request from <" + socket.getSession.getDestination + ">")
@@ -29,14 +32,31 @@ object Server extends App {
   //      writer.write(buffer, 0, read)
   //  }
 
-  val server = actorOf[Server]
-  server.start()
-  server ! "connect"
+//  val server = actorOf[Server]
+//  server.start()
+//  server ! "connect"
+
+  println(System.getProperty("os.name"))
+
+	/** The Constant OS_ARCH. */
+  val arch = "os.arch"
+	println(System.getProperty(arch))
+
+  System.setProperty(arch,"x86_64")
+  println(System.getProperty(arch))
+
+  //TODO from config
+  val socket = new SocketUDT(TypeUDT.DATAGRAM)
+  val id = socket.getSocketId
+
+
+
+
+
 }
 
 class Server extends Actor {
   val handler = actorOf[Handler]
-  lazy val serverSocket = new UDTServerSocket(InetAddress.getByName("localhost"), 65321)
 
   override def preStart() {
     handler.start()
@@ -49,8 +69,7 @@ class Server extends Actor {
   def receive = {
     case "connect" =>
       EventHandler.info(this, "Connected")
-            handler ! serverSocket.accept
-      self ! "connect"
+//      self ! "connect"
     case msg => EventHandler.info(this, "Received unknown message: " + msg)
   }
 }
@@ -64,32 +83,7 @@ class Handler extends Actor with RoundRobinSelector with FixedCapacityStrategy {
   def limit = 2
 
   protected def receive = {
-    case socket: UDTSocket =>
-      EventHandler.info(this, "UDTSocket: " + socket)
-      val in = socket.getInputStream
-      val out = socket.getOutputStream
-      val buf = ListBuffer[Byte]()
-      val b = in.read()
-      while (b >= 0 && b != 10) {
-        println(b)
-        buf += b.toByte
-      }
-
-      val bytes = buf.toArray
-
-
-      println(bytes.mkString("\n"))
-      out.write(bytes)
-      out.flush()
-//      Source.fromInputStream(in).getLines().map{
-//        line =>
-//        println("ECHO: " + line)
-//        writer.w(line)
-//        writer.flush
-//      }
-      println("Request from <" + socket.getSession.getDestination + "> finished.")
-
-    case msg => EventHandler.info(this, "Received unknown message: " + msg)
+     case msg => EventHandler.info(this, "Received unknown message: " + msg)
   }
 
 }
